bd1281dc0bd477a11ee0653e95150c7820df8b83
Marco Tillemans <marcotillemans@gmail.com>
2016-07-05 00:52:07 +0200

    bug fixes, transitioning between CW and CCW path
    transition between CW and CCW path is now correct. Also elaborated on
    example 2 for demonstrating how to use helper functions

--- morph.js
+++ morph.js
@@ -5,7 +5,9 @@ function getNewMorphInstance()
     instance.forward = true;
     instance.times = 0;
     instance.debug = true;
-    instance.distanceBetweenSamples = 0;
+    instance.distanceBetweenSamples = 4; //default value
+    instance.originalWinding=[];
+    instance.invertedY = true; //svg normally have origin in top left corner
 
     /*
         pointsInArray is a array of points in the form {x:int,y:int}
@@ -42,19 +44,28 @@ function getNewMorphInstance()
         path0Data, d value of from path
         path1Data, d value of to path
         pathElement the actual path element in the dom wrapped in d3.select
-        startData, value map {}, which consists of
-                points:[{x,y}], these points are control points which are mapped between shapes
-                the points need to be layed out in order, starting from the first control point in a CW or CCW direction to the other points.
+        startDataPoints0, control points from first path
+        startDataPoints1, control points from second path
+        invertedY, default is true, set to false when using a custom transform, i.e. arcs that are positioned around a center point
+        otherOptions, not yet used
     */
 
-    instance.initialize = function(distanceBetweenSamples, path0Data, path1Data, pathElement, startData0, startData1)
+    instance.initialize = function(distanceBetweenSamples, path0Data, path1Data, pathElement, startDataPoints0, startDataPoints1, invertedY, otherOptions)
     {
+        if (startDataPoints0.length != startDataPoints1.length )
+        {
+            console.log('pathAnimation: controlPoints should have the same number of elements');
+            return;
+        }
+
         instance.pathElement = pathElement;
 
         instance.path0Data = path0Data;
         instance.path1Data = path1Data;
 
-        instance.distanceBetweenSamples;
+        instance.distanceBetweenSamples = (distanceBetweenSamples !== undefined) ? distanceBetweenSamples : instance.distanceBetweenSamples;
+
+        instance.invertedY = (invertedY !== undefined) ? invertedY : instance.invertedY;
 
         var path0 = document.createElementNS("http://www.w3.org/2000/svg", 'path');
         path0.setAttribute("d", instance.path0Data);
@@ -66,7 +77,7 @@ function getNewMorphInstance()
 
         var distances0, distances1;
 
-        distances0 = [0], i = 0, dt = distanceBetweenSamples / Math.max(n0, n1);
+        distances0 = [0], i = 0, dt = instance.distanceBetweenSamples / Math.max(n0, n1);
         while ((i += dt) < 1) distances0.push(i);
         distances0.push(1);
 
@@ -88,37 +99,44 @@ function getNewMorphInstance()
         distances1 = distances0;
 
         //check windings
-        var winding0 = instance.checkWindingArray(instance.points0, true);
-        var winding1 = instance.checkWindingArray(instance.points1, true);
+        instance.originalWinding[0] = instance.checkWindingArray(instance.points0, instance.invertedY);
+        instance.originalWinding[1] = instance.checkWindingArray(instance.points1, instance.invertedY);
 
-        if (!winding0) //false = CCW
+
+        if (!instance.originalWinding[0]) //false = CCW
         {
-            instance.reverseWindingArray(instance.points0);
+            //startDataPoints0 = instance.reverseControlPoints(startDataPoints0);
+            instance.reverseWindingArray(instance.points0,true);
         }
 
-        if (!winding1)
+        if (!instance.originalWinding[1])
         {
-            instance.reverseWindingArray(instance.points1);
+            //startDataPoints1 = instance.reverseControlPoints(startDataPoints1);
+            instance.reverseWindingArray(instance.points1,true);
         }
 
-        if (startData0.points.length != startData1.points.length )
-        {
-            console.log('pathAnimation: controlPoints should have the same number of elements')
-        }
-
-        var indexes0 = instance.findNearestPointInArray(instance.points0, startData0.points);
-        var indexes1 = instance.findNearestPointInArray(instance.points1, startData1.points);
+        var indexes0 = instance.findNearestPointInArray(instance.points0, startDataPoints0);
+        var indexes1 = instance.findNearestPointInArray(instance.points1, startDataPoints1);
 
         instance.points0 = instance.reorderArray(indexes0[0],instance.points0);
         instance.points1 = instance.reorderArray(indexes1[0],instance.points1);
 
         //since the points of the path can be reorderd, get pointAtLength would give incorrect result, startPointAddition is the offset for getting correct values out of pointAtLength
-        var startPointAddition = [distances0[indexes0[0]], distances1[indexes1[0]]];
 
+        var startPointAddition = [
+            (instance.originalWinding[0]) ? distances0[indexes0[0]] : 1 - distances0[indexes0[0]],
+            (instance.originalWinding[1]) ? distances1[indexes1[0]] : 1 - distances1[indexes1[0]]
+        ];
         var newIndexes0 = instance.moveArrayIndexes(instance.points0, indexes0);
         var newIndexes1 = instance.moveArrayIndexes(instance.points1, indexes1);
 
-        instance.mapArrayFromPointToPoint([newIndexes0, newIndexes1], [instance.points0, instance.points1],[path0, path1], [distances0, distances1], [n0,n1], startPointAddition, (n0 > n1) ? 0 : 1);
+        instance.mapArrayFromPointToPoint([newIndexes0, newIndexes1], [indexes0, indexes1], [instance.points0, instance.points1],[path0, path1], [distances0, distances1], [n0,n1], startPointAddition, (n0 > n1) ? 0 : 1);
+
+        if (instance.points0.length != instance.points1.length)
+        {
+            console.log('inequal number of points no mapping possible, check the shapes and control points');
+            return;
+        }
 
         instance.points = [];
 
@@ -127,6 +145,7 @@ function getNewMorphInstance()
         {
             instance.points.push(d3.interpolate([instance.points0[i].x,instance.points0[i].y],[instance.points1[i].x,instance.points1[i].y]));
         }
+
     };
     instance.reorderArray = function(fromIndex, array)
     {
@@ -175,7 +194,7 @@ function getNewMorphInstance()
         return indexes;
     };
     //mapping longest path on shortest path
-    instance.mapArrayFromPointToPoint = function (indexes, points, paths, distances, totalLenghts, startPointAddition, longerPathIndex)
+    instance.mapArrayFromPointToPoint = function (indexes, originalIndexes, points, paths, distances, totalLenghts, startPointAddition, longerPathIndex)
     {
         var addIndexes = [ [] , [] ];
         var dif = [];
@@ -187,8 +206,13 @@ function getNewMorphInstance()
         points[1].push(points[1][0]);
         indexes[0].push(points[0].length - 1);
         indexes[1].push(points[1].length - 1);
+        originalIndexes[0].push(points[0].length - 1);
+        originalIndexes[1].push(points[1].length - 1);
         distances[0].push(1);
         distances[1].push(1);
+
+        var smaller = (longerPathIndex == 0) ? 1 : 0; //(dif[0] < dif[1]) ? 0 : 1;
+        var bigger = longerPathIndex; //(dif[0] > dif[1]) ? 0 : 1;
 
         for (var i = 0; i < indexes[0].length -1; i++)
         {
@@ -212,42 +236,52 @@ function getNewMorphInstance()
             dif[0] = end[0] - start[0];
             dif[1] = end[1] - start[1];
 
-            if (instance.debug) console.log('dif0 ' + dif[0] + ' dif1 ' + dif[1]);
+            if (instance.debug) console.log('dif0 ' + dif[0] + ' dif1 ' + dif[1] + ' curIndex ' + curIndex);
 
             //when number differs remove points in between control points of the shortest path and create new points according to points in between the control points of the longer path
             if (dif[0] != dif[1])
             {
-
-                var smaller = (longerPathIndex == 0) ? 1 : 0; //(dif[0] < dif[1]) ? 0 : 1;
-                var bigger = longerPathIndex; //(dif[0] > dif[1]) ? 0 : 1;
+                bigger = (dif[0] > dif[1]) ? 0 : 1;
+                smaller = (bigger == 0) ? 1 :0;
 
                 var curDif = dif[bigger] -1; //don't include two control points
+                if (instance.debug) console.log('remove  index' + (curIndex + 1) + ' amount ' + (dif[smaller] - 1));
+                if (instance.debug) console.log('adding ' + (dif[bigger] - 1) + ' at ' + (curIndex + 1) + ' smaller ' + smaller + ' bigger ' + bigger);
 
-                if (instance.debug) console.log('adding ' + (dif[bigger] - 1));
+                var startLength, endLength;
+                if (instance.originalWinding[smaller])
+                {
+                    startLength = distances[smaller][indexes[smaller][i]] + startPointAddition[smaller];
+                    endLength = distances[smaller][indexes[smaller][i+1]] + startPointAddition[smaller];
+                }
+                else
+                {
+                    startLength = startPointAddition[smaller] - distances[smaller][indexes[smaller][i]];
+                    endLength = startPointAddition[smaller] - distances[smaller][indexes[smaller][i+1]];
 
-                //WARNING this only works for CW paths, for CCW startPointAddition needs to be substracted
-                var startLength = distances[smaller][indexes[smaller][i]] + startPointAddition[smaller];
-                var endLength = distances[smaller][indexes[smaller][i+1]] + startPointAddition[smaller];
+                    startLength = instance.normalizePathDistance(startLength);
+                    endLength = instance.normalizePathDistance(endLength);
 
+                }
+                if (instance.debug) console.log('startLength ' + startLength + ' endLength ' + endLength);
                 var pathLengthPortion = ( endLength - startLength) / curDif;
 
                 //remove current points
                 instance.removeElements(points[smaller], curIndex + 1, Math.max(0,dif[smaller] - 1));
-
-                if (instance.debug) console.log('remove  index' + (curIndex + 1) + ' amount ' + (dif[smaller] - 1));
 
                 //add elements
                 var tempArray = [];
                 var startIndex = curIndex + 1;
                 for (var j = 1; j <= curDif; j++)
                 {
-
                     var point = paths[smaller].getPointAtLength( instance.normalizePathDistance(startLength + j * pathLengthPortion) * totalLenghts[smaller]);
                     tempArray.push(point);
                     curIndex += 1;
                 }
 
-                instance.insertItemAtIndex(tempArray,startIndex,points[smaller])
+                instance.insertItemAtIndex(tempArray,startIndex,points[smaller]);
+
+                if (instance.debug) console.log('points length ' + points[0].length + ' ' + points[1].length);
 
                 curIndex += 1; //for 2nd control point
             }
@@ -258,10 +292,11 @@ function getNewMorphInstance()
         }
     };
     //used for converting new path distance (starting at control point 0) to old path distance
-    instance.normalizePathDistance= function(number)
+    instance.normalizePathDistance = function(number)
     {
         if (number > 1) return number -1;
-        else return number;
+        else if (number < 0) return number + 1;
+        return number;
     }
     instance.removeElements = function(array,startIndex, howMany)
     {
@@ -331,6 +366,16 @@ function getNewMorphInstance()
             array.splice(index, 0, item);
         }
     };
+    instance.reverseControlPoints = function(controlPoints)
+    {
+        var newArray = [];
+        newArray.push(controlPoints[0]);
+        for (var i = controlPoints.length - 1; i >= 1; i--)
+        {
+            newArray.push(controlPoints[i]);
+        }
+        return newArray;
+    };
     instance.startAnimation = function(duration, delay, easeFunction, functionToCall)
     {
         instance.functionToCall = (functionToCall !== undefined) ? functionToCall : null;
@@ -341,8 +386,16 @@ function getNewMorphInstance()
             {
                 instance.functionToCall();
             }
-
         });
+    };
+    instance.tweenFunction = function ()
+    {
+        return function()
+        {
+            return function(t) {
+                return t < 1 ? "M" + instance.points.map(function(p) { return p(t); }).join("L") : (instance.forward) ? instance.path1Data : instance.path0Data;
+            };
+        }
     };
     instance.reverse = function ()
     {
@@ -357,15 +410,6 @@ function getNewMorphInstance()
             instance.points.push(d3.interpolate([from[i].x,from[i].y],[to[i].x,to[i].y]));
         }
     };
-    instance.tweenFunction = function ()
-    {
-        return function()
-        {
-            return function(t) {
-                return t < 1 ? "M" + instance.points.map(function(p) { return p(t); }).join("L") : (instance.forward) ? instance.path1Data : instance.path0Data;
-            };
-        }
-    };
     instance.circleToPath = function(r)
     {
         var output = "M" + (-r).toString() + ",0"; //+ (r).toString();
@@ -376,50 +420,63 @@ function getNewMorphInstance()
         return output;
     };
     //element is d3 selection of element
-    instance.displayPointArray = function(pointArray, element, showNumber)
+    instance.displayPointArray = function(pointArray, element, showNumber,skipAmount,tillIndex)
     {
-        for (var i = 0; i < pointArray.length; i++)
+        var end = (tillIndex !== undefined) ? tillIndex + 1: pointArray.length;
+        var skipNumbers = (skipAmount !== undefined) ? skipAmount : 1;
+        for (var i = 0; i < end ; i++)
         {
             element.append('circle').attr({cx:pointArray[i].x,cy: pointArray[i].y, r: 1}).classed('testPoint',true);
-            if (showNumber)
+            if (showNumber && i % skipNumbers == 0)
             {
-                element.append('text').attr({x:pointArray[i].x + 6, y: pointArray[i].y + 6})
+                element.append('text').attr({x:pointArray[i].x + 3, y: pointArray[i].y + 5})
                 .text(i)
                 .attr("font-family", "sans-serif")
-                .attr("font-size", "12px")
+                .attr("font-size", "10px")
                 .attr("fill", "green");
             }
         }
-    }
+    };
+    //element is dom element, for d3 element use el.node()
     instance.getSegmentCoordinates = function(element)
     {
-        var pathSegList = element.node().pathSegList;
+        var pathSegList = element.pathSegList;
 
         var dataset = [];
 
         // loop through segments, adding each endpoint to the restored dataset
+        var curPoint = null;
         for (var i = 0; i < pathSegList._list.length; i++) {
             var item = pathSegList.getItem(i);
-            if (item.x !== undefined)
+
+            if (/^[A-Z]/.test(item.pathSegTypeAsLetter))
             {
-                dataset.push({
+                curPoint = {
                     x: instance.round(item.x),
                     y: instance.round(item.y)
-                })
+                };
+                dataset.push(curPoint);
             }
-            else
+            else if (item.pathSegTypeAsLetter != 'z')
             {
-                if (instance.debug) console.log('encountered ' + item.pathSegTypeAsLetter + ' without position');
+                curPoint.x += instance.round(item.x);
+                curPoint.y += instance.round(item.y);
+
+                dataset.push({
+                    x: curPoint.x,
+                    y: curPoint.y
+                });
             }
+
         }
 
         return dataset;
-    }
+    };
 
     instance.round = function(aNumber)
     {
         return Math.round((aNumber + 0.0001) * 100 ) / 100;
-    }
+    };
 
     return instance;
 }
